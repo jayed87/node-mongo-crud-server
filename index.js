d67e40f8a0eae1ecfe36cc52a9e7d89b2f2d4b65
JayedAkbarSumon <akbarjayed87@gmail.com>
2021-12-20 08:34:53 +0600

    update added in server

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         });
+        // GET specific user
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const user = await usersCollection.findOne(query);
+            console.log('load user with', id);
+            res.send(user);
+        })
         // POST api 
         app.post('/users', async (req, res) => {
             const newUser = req.body;
